d76bb3cfc78d59dcea7879abee91cefee2b4d1a9
korayzun <syneapp22@gmail.com>
2024-11-06 12:09:38 +0100

    added logs in index.js

--- functions/index.js
+++ functions/index.js
@@ -14,6 +14,30 @@
 //   });
 // });
 
+// const functions = require("firebase-functions");
+// const cors = require("cors");
+
+// const corsOptions = {
+//   origin: "https://korayzun.github.io",
+//   methods: ["GET"],
+//   allowedHeaders: ["Content-Type"],
+// };
+
+// exports.getAppScriptUrl = functions.https.onRequest((req, res) => {
+//   cors(corsOptions)(req, res, () => {
+//     if (req.method === "GET") {
+//       const appScriptUrl = process.env.APPSCRIPT_URL;
+//       if (!appScriptUrl) {
+//         return res.status(500).send("App Script URL not set.");
+//       }
+
+//       res.status(200).json({url: appScriptUrl});
+//     } else {
+//       return res.status(405).send("Method Not Allowed");
+//     }
+//   });
+// });
+
 const functions = require("firebase-functions");
 const cors = require("cors");
 
@@ -24,16 +48,32 @@ const corsOptions = {
 };
 
 exports.getAppScriptUrl = functions.https.onRequest((req, res) => {
-  cors(corsOptions)(req, res, () => {
-    if (req.method === "GET") {
+  cors(corsOptions)(req, res, async () => {
+    try {
+      if (req.method !== "GET") {
+        console.error("Invalid request method:", req.method);
+        return res.status(405).send("Method Not Allowed");
+      }
+
+      // Logging the request details for debugging
+      console.log("Request received from origin:", req.headers.origin);
+
+      // Retrieve App Script URL from environment variable
       const appScriptUrl = process.env.APPSCRIPT_URL;
+
       if (!appScriptUrl) {
+        console.error("App Script URL is not set in environment variables.");
         return res.status(500).send("App Script URL not set.");
       }
 
+      console.log("App Script URL retrieved successfully:", appScriptUrl);
+
+      // Respond with the URL in JSON format
       res.status(200).json({url: appScriptUrl});
-    } else {
-      return res.status(405).send("Method Not Allowed");
+    } catch (error) {
+      console.error("Error occurred in getAppScriptUrl function:", error);
+      res.status(500).send("Internal Server Error: " + error.message);
     }
   });
 });
+
