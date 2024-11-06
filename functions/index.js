1db4cc8231da7419143ec2c972f0b29ebc1a6734
korayzun <syneapp22@gmail.com>
2024-11-06 11:15:37 +0100

    index.js has cors now

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,18 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 const functions = require("firebase-functions");
+const cors = require("cors")({origin: "https://korayzun.github.io"});
 
 // Fetch the Apps Script URL from Firebase Functions Config
 exports.getAppScriptUrl = functions.https.onRequest((req, res) => {
-  const appScriptUrl = functions.config().appscript.url;
+  // CORS pre-flight request
+  cors(req, res, () => {
+    const appScriptUrl = functions.config().appscript.url;
 
-  if (!appScriptUrl) {
-    return res.status(500).send("App Script URL not set.");
-  }
+    if (!appScriptUrl) {
+      return res.status(500).send("App Script URL not set.");
+    }
 
-  res.status(200).json({url: appScriptUrl});
+    // Send the Apps Script URL to the client
+    res.status(200).json({url: appScriptUrl});
+  });
 });
 
