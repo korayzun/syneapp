00fdb2429f82584858d8f37bd8054594be7241ad
korayzun <syneapp22@gmail.com>
2024-11-06 11:25:18 +0100

    Changed to env variables

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,15 @@
 const functions = require("firebase-functions");
 const cors = require("cors")({origin: "https://korayzun.github.io"});
 
-// Fetch the Apps Script URL from Firebase Functions Config
 exports.getAppScriptUrl = functions.https.onRequest((req, res) => {
-  // CORS pre-flight request
   cors(req, res, () => {
-    const appScriptUrl = functions.config().appscript.url;
+    // Access the App Script URL via environment variables
+    const appScriptUrl = process.env.APPSCRIPT_URL;
 
     if (!appScriptUrl) {
       return res.status(500).send("App Script URL not set.");
     }
 
-    // Send the Apps Script URL to the client
     res.status(200).json({url: appScriptUrl});
   });
 });
-
